3dfd45bd969b81ac28b057e2e61aacdc4d0360f5
sakshi-011 <sakshi.singhania011@gmail.com>
2025-04-24 19:42:14 +0530

    Coding Exercise

---
+++ src/main/java/Udemy_Java_Masterclass_2025/CodingExercise9.java
@@ -0,0 +1,16 @@
+package main.java.Udemy_Java_Masterclass_2025;
+
+public class CodingExercise9 {
+
+    public static double area(double radius){
+        if(radius < 0)
+            return -1.0;
+        return Math.PI * radius * radius;
+    }
+
+    public static double area(double x, double y){
+        if(x < 0 || y < 0)
+            return -1.0;
+        return x * y;
+    }
+}
