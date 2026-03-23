b8e6f78e362745e95287f3ed2f153599e8a28715
sakshi-011 <sakshi.singhania011@gmail.com>
2026-03-23 21:03:12 +0530

    Coding Exercise

---
+++ src/main/java/Udemy_Java_Masterclass_2025/CodingExercise29.java
@@ -0,0 +1,38 @@
+package main.java.Udemy_Java_Masterclass_2025;
+
+import java.util.Scanner;
+
+public class CodingExercise29 {
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <=0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0)
+            return -1;
+        double area = width * height;
+        int noOfBuckets = (int)Math.ceil(area / areaPerBucket);
+        return noOfBuckets - extraBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if(width <=0 || height <= 0 || areaPerBucket <= 0)
+            return -1;
+        double area = width * height;
+        int noOfBuckets = (int)Math.ceil(area / areaPerBucket);
+        return noOfBuckets;
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <= 0)
+            return -1;
+        int noOfBuckets = (int)Math.ceil(area / areaPerBucket);
+        return noOfBuckets;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
+    }
+}
